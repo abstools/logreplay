cd91e63e282880a168b1f4e29a88386009f3a4af
Behrooz Nobakht <nobeh5@gmail.com>
2016-08-09 10:36:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# logreplay
+
+An experimental research tool to replay a log file as a series of queries onto an RESTful endpoint with a lot of assumptions and conventions.
+
+To run the tool:
+
+```bash
+$ python logreplay.py /path/log-file param1,param2 extra1=1,extra2=2 URL /QueryPath
+```
+
+in which:
+
+* `paramN` is the name of parameter to extract from log file
+* `extraX=Y` is an extra parameter key-value to send to the endpoint
+* `URL` is the RESTful endpoint to receive the queries extracted from the log file; e.g. `http://httpbin.org`.
+* `/QueryPath` is the endpoint receiving queries; e.g. `/get`
+* The log file is assumed to have the following structure on every line:
+
+```
+DATE TIME LEVEL [EXTRA] [MESSAGE]
+```
+
+  * `DATE TIME` are in standard Python format `%Y-%m-%d %H:%M:%S,%f`.
+  * The last *space-separated* part of each log line is considered as the *message*. The message may contain character `&` to denote standard HTTP query parameters.
+  * All the log line elements in between, including `LEVEL` or others, are ignored.
---
+++ logreplay.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+from datetime import datetime
+import logging
+import re
+import requests
+import sys
+import time
+
+def load_log(log_file):
+	lines = {}
+	for line in open(log_file, 'r'):
+		parts = line.split(" ")
+		log_time = datetime.strptime(parts[0] + " " + parts[1], '%Y-%m-%d %H:%M:%S,%f')
+		# Assume that the last part of a log line is the data part
+		log_query = parts[-1]
+		lines[log_time] = log_query
+	return lines
+
+def filter_logs(logs, target_params, extra_params):
+	if len(target_params) == 0:
+		return logs
+	filtered_logs = {}
+	for k, v in logs.iteritems():
+		# val can be 'id&' or 'id=xxx&'
+		filtered_values = [val for val in v.split("&") if val in target_params or val[:val.index('=')] in target_params]
+		filtered_logs[k] = "&".join(filtered_values + extra_params)
+	return filtered_logs
+
+def send_queries(logs, url, query_path):
+	times = sorted(list(logs.keys()))
+	delays = build_delays(times)
+	logging.info("Using delays in milliseconds: %s", delays)
+	for log_time in times:
+		params = logs[log_time]
+		status_code = send_query(params, url, query_path)
+		if status_code != 200:
+			logging.error("Request failed: %s -> %s", response.status_code, params)
+		else:
+			logging.info("200 %s", params)
+		if (len(delays) > 0):
+			delay = delays.pop(0)
+			logging.info("Waiting %s msec(s) for next query ...", delay)
+			time.sleep(delay / 1000.0)
+
+def send_query(query, url = "http://httpbin.org", path = "/get"):
+	response = requests.get(url + path, data = query)
+	return response.status_code
+
+def build_delays(times):
+	t_0 = times[0]
+	delays = []
+	for t in times:
+		delta = t - t_0
+		delta_ms = delta.microseconds / 1000.0
+		delays.append(delta_ms)
+	# First query does not wait; assert delays[0] == 0
+	delays.pop(0)
+	return delays
+
+if __name__ == "__main__":
+	if (len(sys.argv) < 5):
+		print "Usage: parse.py LOG_FILE TARGET_PARAMS EXTRA_PARAMS URL QUERY_PATH"
+		sys.exit(1)
+
+	log_file = sys.argv[1]
+	target_params = sys.argv[2].split(",")
+	extra_params = sys.argv[3].split(",")
+	url = sys.argv[4]
+	query_path = sys.argv[5]
+
+	logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+	
+	lines = load_log(log_file)	
+	logging.info("Loaded log file. Size: %s", len(lines))
+
+	logging.info("Using extr query parameters: %s", extra_params)
+	logging.info("Using target query parameters: %s", target_params)
+	logs = filter_logs(lines, target_params, extra_params)
+	logging.info("Filtered logs. Size: %s", len(logs))
+
+	send_queries(logs, url, query_path)
