6b555159dd0d211a9954d6926b330208aa9c4d0c
Behrooz Nobakht <nobeh5@gmail.com>
2016-08-16 18:16:53 +0200

    Improve code quality based on pylint

--- custom_filters.py
+++ custom_filters.py
@@ -1,22 +1,35 @@
-import logging
+"""
+The Python script file that contains all
+custom filter functions.
+"""
 
 def apply_filter(value):
-	return value
+    """A do-nothing custom filter that returns
+	the value of the input as-is.
 
-def apply_filter(value):
-	key = ""
-	val = value
-	idx = value.index('=')
-	is_key_value = idx > 0
-	if is_key_value:
-		key = value[:idx]
-		val = value[idx + 1:]
-	try:
-		v = float(val)
-		final_val = str(int(v) + 1)
-		final_key_value = key + "=" + final_val if is_key_value else final_val
-		logging.debug("%s -> %s", value , final_key_value)
-		return final_key_value
-	except:
-		# Ignore everything, just return the original value
-		return value
+	value -- the value to be filtered
+	"""
+    return value
+
+# def apply_filter(value):
+# 	"""A custom filter that converts a double value
+# 	to an integer value.
+
+# 	value -- the value to filter that can be as 'key=value'
+# 	"""
+# 	key = ""
+# 	val = value
+# 	idx = value.index('=')
+# 	is_key_value = idx > 0
+# 	if is_key_value:
+# 		key = value[:idx]
+# 		val = value[idx + 1:]
+# 	try:
+# 		v = float(val)
+# 		final_val = str(int(v) + 1)
+# 		final_key_value = key + "=" + final_val if is_key_value else final_val
+# 		logging.debug("%s -> %s", value , final_key_value)
+# 		return final_key_value
+# 	except:
+# 		# Ignore everything, just return the original value
+# 		return value
--- logreplay.py
+++ logreplay.py
@@ -1,90 +1,139 @@
 #!/usr/bin/python
+"""
+A log reply tool
+"""
 
-from custom_filters import *
 from datetime import datetime
 import logging
-import os
-import re
-import requests
 import sys
 import time
+import requests
+from custom_filters import *
+
 
 def load_log(log_file):
-	lines = {}
-	for line in open(log_file, 'r'):
-		parts = line.split(" ")
-		log_time = datetime.strptime(parts[0] + " " + parts[1], '%Y-%m-%d %H:%M:%S,%f')
-		# Assume that the last part of a log line is the data part
-		log_query = parts[-1]
-		lines[log_time] = log_query
-	return lines
+    """Read the log file and return lines as a dictionary
+
+	log_file -- the path to the log file
+	"""
+    file_lines = {}
+    for line in open(log_file, 'r'):
+        parts = line.split(" ")
+        log_time = datetime.strptime(parts[0] + " " + parts[1],
+                                     '%Y-%m-%d %H:%M:%S,%f')
+        # Assume that the last part of a log line is the data part
+        log_query = parts[-1]
+        file_lines[log_time] = log_query
+    return file_lines
+
 
 def filter_logs(logs, target_params, extra_params):
-	if len(target_params) == 0:
-		return logs
-	filtered_logs = {}
-	for k, v in logs.iteritems():
-		# val can be 'id&' or 'id=xxx&'
-		filtered_values = [val for val in v.split("&") if val in target_params or val[:val.index('=')] in target_params]
-		values = apply_custom_filters(filtered_values)
-		filtered_logs[k] = "&".join(values + extra_params)
-	return filtered_logs
+    """Filter a logs dictionary based input target parameters.
+
+    logs -- the input logs dictionary
+    target_params -- the interested set of input parameter keys
+    extra_params -- the list of parameters to be added input from command line
+    """
+    if len(target_params) == 0:
+        return logs
+    filtered_logs = {}
+    for key, value in logs.iteritems():
+        # val can be 'id&' or 'id=xxx&'
+        filtered_values = [val for val in value.split("&") \
+         if val in target_params or val[:val.index('=')] in target_params]
+        values = apply_custom_filters(filtered_values)
+        filtered_logs[key] = "&".join(values + extra_params)
+    return filtered_logs
+
 
 def apply_custom_filters(values):
-	filtered_values = [apply_filter(v) for v in values]
-	return filtered_values
+    """Apply custom key/value filter functions loaded
+    dynamically from an external file 'custom_filters.py'
+
+    values -- the list of values to be filtered
+    """
+    filtered_values = [apply_filter(v) for v in values]
+    return filtered_values
+
 
 def send_queries(logs, url, query_path):
-	times = sorted(list(logs.keys()))
-	delays = build_delays(times)
-	logging.info("Using delays in milliseconds: %s", delays)
-	for log_time in times:
-		params = logs[log_time]
-		status_code = send_query(params, url, query_path)
-		if status_code != 200:
-			logging.error("Request failed: %s -> %s", status_code, params)
-		else:
-			logging.info("200 %s", params)
-		if (len(delays) > 0):
-			delay = delays.pop(0)
-			logging.info("Waiting %s msec(s) for next query ...", delay)
-			time.sleep(delay / 1000.0)
+    """Send logs as HTTP requests to an endpoint
 
-def send_query(query, url = "http://httpbin.org", path = "/get"):
-	response = requests.get(url + path, params = query)
-	logging.debug("Completed: %s", response.url)
-	return response.status_code
+    logs -- the dictionary of logs
+    url -- the endpoint that receives HTTP requests
+    query_path -- the HTTP query path to use for every request as prefix
+    """
+    times = sorted(list(logs.keys()))
+    delays = build_delays(times)
+    logging.info("Using delays in milliseconds: %s", delays)
+    for log_time in times:
+        params = logs[log_time]
+        status_code = send_query(params, url, query_path)
+        if status_code != 200:
+            logging.error("Request failed: %s -> %s", status_code, params)
+        else:
+            logging.info("200 %s", params)
+        if len(delays) > 0:
+            delay = delays.pop(0)
+            logging.info("Waiting %s msec(s) for next query ...", delay)
+            time.sleep(delay / 1000.0)
+
+
+def send_query(query, url="http://httpbin.org", path="/get"):
+    """Send a single HTTP query to an endpoint
+
+    query -- the query string for the HTTP request
+    url -- the endpoint URL
+    path -- the endpoint prefix path
+    """
+    response = requests.get(url + path, params=query)
+    logging.debug("Completed: %s", response.url)
+    return response.status_code
+
 
 def build_delays(times):
-	t_0 = times[0]
-	delays = []
-	for t in times:
-		delta = t - t_0
-		delta_ms = delta.microseconds / 1000.0
-		delays.append(delta_ms)
-	# First query does not wait; assert delays[0] == 0
-	delays.pop(0)
-	return delays
+    """Build a list of delays in mill-seconds from the times
+    of logs loaded from the file
+
+    times -- the list of log time stamps loaded from the log file
+    """
+    t_0 = times[0]
+    delays = []
+    for log_t in times:
+        delta = log_t - t_0
+        delta_ms = delta.microseconds / 1000.0
+        delays.append(delta_ms)
+    # First query does not wait; assert delays[0] == 0
+    delays.pop(0)
+    return delays
+
+def main(args):
+    """The main function
+
+    args -- the program arguments
+    """
+    log_file = args[1]
+    target_params = args[2].split(",")
+    extra_params = args[3].split(",")
+    url = args[4]
+    query_path = args[5]
+
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+    lines = load_log(log_file)
+    logging.info("Loaded log file. Size: %s", len(lines))
+
+    logging.info("Using extr query parameters: %s", extra_params)
+    logging.info("Using target query parameters: %s", target_params)
+    logs = filter_logs(lines, target_params, extra_params)
+    logging.info("Filtered logs. Size: %s", len(logs))
+
+    send_queries(logs, url, query_path)
 
 if __name__ == "__main__":
-	if (len(sys.argv) < 5):
-		print "Usage: logreplay.py LOG_FILE TARGET_PARAMS EXTRA_PARAMS URL QUERY_PATH"
-		sys.exit(1)
+    if len(sys.argv) < 5:
+        print "Usage: logreplay.py LOG_FILE TARGET_PARAMS EXTRA_PARAMS URL QUERY_PATH"
+        sys.exit(1)
 
-	log_file = sys.argv[1]
-	target_params = sys.argv[2].split(",")
-	extra_params = sys.argv[3].split(",")
-	url = sys.argv[4]
-	query_path = sys.argv[5]
-
-	logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
-	
-	lines = load_log(log_file)	
-	logging.info("Loaded log file. Size: %s", len(lines))
-
-	logging.info("Using extr query parameters: %s", extra_params)
-	logging.info("Using target query parameters: %s", target_params)
-	logs = filter_logs(lines, target_params, extra_params)
-	logging.info("Filtered logs. Size: %s", len(logs))
-
-	send_queries(logs, url, query_path)
+    main(sys.argv)
